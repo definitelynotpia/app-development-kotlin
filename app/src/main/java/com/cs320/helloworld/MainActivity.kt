a69a14560ca2f9102429090c70a1cd081c7d9297
Frances Pia Alexy Rey <frances.rey@ciit.edu.ph>
2024-01-11 20:40:35 +0800

    1/11/2024 Th 8:39p - MainActivity and Login UI, button redirect for Login & Register with Intent

--- app/src/main/java/com/cs320/helloworld/MainActivity.kt
+++ app/src/main/java/com/cs320/helloworld/MainActivity.kt
@@ -2,10 +2,26 @@ package com.cs320.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val loginBtn = findViewById<Button>(R.id.mainLogin)
+
+        val registerBtn = findViewById<Button>(R.id.mainRegister)
+
+        loginBtn.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+
+        registerBtn.setOnClickListener {
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
+        }
     }
 }
