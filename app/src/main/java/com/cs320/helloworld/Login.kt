168fa482cf690a14e50b505a6877cf60be5d51a6
Frances Pia Alexy Rey <frances.rey@ciit.edu.ph>
2024-01-18 20:43:44 +0800

    1/18/2024 Th 8:42p - responsive checkbox and radio buttons in both Login and Register forms.

--- app/src/main/java/com/cs320/helloworld/Login.kt
+++ app/src/main/java/com/cs320/helloworld/Login.kt
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.TextView
+import android.widget.EditText
 import android.widget.Toast
 
 class Login : AppCompatActivity() {
@@ -12,13 +14,48 @@ class Login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        val username = findViewById<EditText>(R.id.loginUsername)
+        val password = findViewById<EditText>(R.id.loginPassword)
+        val rememberMe = findViewById<CheckBox>(R.id.rememberMe)
         val loginButton = findViewById<Button>(R.id.login_button)
-        val registerButton = findViewById<TextView>(R.id.redirect_register)
-        val forgotPasswordButton = findViewById<TextView>(R.id.forgot_password)
+
+        rememberMe.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                Toast.makeText(this, "We will remember your account", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                Toast.makeText(this, "We won't remember your account", Toast.LENGTH_SHORT).show()
+            }
+        }
 
         loginButton.setOnClickListener {
-            // TODO Auto-generated constructor stub
+            val usernameSubmitted = username.text.toString()
+            val passwordSubmitted = password.text.toString()
+
+            // Check if username and password are correct
+            if (usernameSubmitted == "admin" && passwordSubmitted == "1234") {
+                // Return success message
+                Toast.makeText(
+                    this,
+                    "Login Successful: $usernameSubmitted, $passwordSubmitted",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                // Redirect to Main Activity
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Login Failed, create an account now!", Toast.LENGTH_SHORT)
+                    .show()
+
+                // Redirect to Register Activity
+                val intent = Intent(this, Register::class.java)
+                startActivity(intent)
+            }
         }
+
+        val registerButton = findViewById<TextView>(R.id.redirect_register)
+        val forgotPasswordButton = findViewById<TextView>(R.id.forgot_password)
 
         // Redirect to Register Activity
         registerButton.setOnClickListener {
@@ -28,7 +65,8 @@ class Login : AppCompatActivity() {
 
         // Display Forgot Password Dialog
         forgotPasswordButton.setOnClickListener {
-            Toast.makeText(this@Login, "Oh, you forgot? Sorry, can't help you!", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this@Login, "Oh, you forgot? Sorry, can't help you!", Toast.LENGTH_SHORT)
+                .show()
         }
 
 
--- app/src/main/java/com/cs320/helloworld/Register.kt
+++ app/src/main/java/com/cs320/helloworld/Register.kt
@@ -3,15 +3,33 @@ package com.cs320.helloworld
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
 import android.widget.TextView
+import android.widget.Toast
 
 class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        val loginButton = findViewById<TextView>(R.id.redirect_login)
-        loginButton.setOnClickListener {
+        val radioButton1 = findViewById<RadioButton>(R.id.radioButton1)
+        val radioButton2 = findViewById<RadioButton>(R.id.radioButton2)
+        val radioButton3 = findViewById<RadioButton>(R.id.radioButton3)
+
+        radioButton1.setOnClickListener {
+            Toast.makeText(this, "You are male.", Toast.LENGTH_SHORT).show()
+        }
+
+        radioButton2.setOnClickListener {
+            Toast.makeText(this, "You are female.", Toast.LENGTH_SHORT).show()
+        }
+
+        radioButton3.setOnClickListener {
+            Toast.makeText(this, "You are neither male nor female.", Toast.LENGTH_SHORT).show()
+        }
+
+        val registerButton = findViewById<TextView>(R.id.redirect_login)
+        registerButton.setOnClickListener {
             val intent = Intent(this, Login::class.java)
             startActivity(intent)
         }
