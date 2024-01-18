9d8616080db7eae6d3869c7fdd8c591ebbfe507e
Frances Pia Alexy Rey <frances.rey@ciit.edu.ph>
2024-01-18 20:22:12 +0800

    1/18/2024 Th 8:20p - refined UI, added clickable text in forms, added checkbox and radio buttons in forms

--- app/src/main/java/com/cs320/helloworld/Login.kt
+++ app/src/main/java/com/cs320/helloworld/Login.kt
@@ -2,10 +2,35 @@ package com.cs320.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val loginButton = findViewById<Button>(R.id.login_button)
+        val registerButton = findViewById<TextView>(R.id.redirect_register)
+        val forgotPasswordButton = findViewById<TextView>(R.id.forgot_password)
+
+        loginButton.setOnClickListener {
+            // TODO Auto-generated constructor stub
+        }
+
+        // Redirect to Register Activity
+        registerButton.setOnClickListener {
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
+        }
+
+        // Display Forgot Password Dialog
+        forgotPasswordButton.setOnClickListener {
+            Toast.makeText(this@Login, "Oh, you forgot? Sorry, can't help you!", Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 }
--- app/src/main/java/com/cs320/helloworld/Register.kt
+++ app/src/main/java/com/cs320/helloworld/Register.kt
@@ -1,11 +1,19 @@
 package com.cs320.helloworld
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val loginButton = findViewById<TextView>(R.id.redirect_login)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
     }
 }
